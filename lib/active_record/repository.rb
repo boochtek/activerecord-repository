105498b409807da5a78b2566a23b2f5dd31793f6
Craig Buchek <cbuchek@weedmaps.com>
2019-04-30 08:11:10 -0500

    Refactor Repository

--- lib/active_record/repository.rb
+++ lib/active_record/repository.rb
@@ -10,53 +10,80 @@ module ActiveRecord
 
   module Repository
 
+    MODULES_EXTENDED = [
+      ActiveModel::Naming,
+      ActiveSupport::Benchmarkable,
+      ActiveSupport::DescendantsTracker,
+      ConnectionHandling,
+      QueryCache::ClassMethods,
+      Querying,
+      Translation,
+      DynamicMatchers,
+      Explain,
+      Enum,
+      Delegation::DelegateCache,
+      CollectionCacheKey,
+    ]
+
+    MODULES_INCLUDED_FIRST = [
+      Core,
+      Persistence,
+      ReadonlyAttributes,
+      ModelSchema,
+      Inheritance,
+    ]
+
+    MODULES_INCLUDED = [
+      Core,
+      Persistence,
+      ReadonlyAttributes,
+      ModelSchema,
+      Inheritance,
+      Scoping,
+      Sanitization,
+      AttributeAssignment,
+      ActiveModel::Conversion,
+      Integration,
+      CounterCache,
+      Attributes,
+      AttributeDecorators,
+      Locking::Optimistic,
+      Locking::Pessimistic,
+      DefineCallbacks,
+      AttributeMethods,
+      Timestamp,
+      Associations,
+      ActiveModel::SecurePassword,
+      AutosaveAssociation,
+      Aggregations,
+      Transactions,
+      Reflection,
+      Serialization,
+      Store,
+      SecureToken,
+      Suppressor,
+      # Validations,
+      # Callbacks,
+      # NestedAttributes,
+      # TouchLater,
+      # NoTouching,
+    ]
+
     def self.included(mod)
-      mod.extend ActiveModel::Naming
-      mod.extend ActiveSupport::Benchmarkable
-      mod.extend ActiveSupport::DescendantsTracker
-      mod.extend ConnectionHandling
-      mod.extend QueryCache::ClassMethods
-      mod.extend Querying
-      mod.extend Translation
-      mod.extend DynamicMatchers
-      mod.extend Explain
-      mod.extend Enum
-      mod.extend Delegation::DelegateCache
-      mod.extend CollectionCacheKey
-      mod.include Core
-      mod.include Persistence
-      mod.include ReadonlyAttributes
-      mod.include ModelSchema
-      mod.include Inheritance
-      mod.extend BaseClassFix
-      mod.include Scoping
-      mod.include Sanitization
-      mod.include AttributeAssignment
-      mod.include ActiveModel::Conversion
-      mod.include Integration
-      # mod.include Validations
-      mod.include CounterCache
-      mod.include Attributes
-      mod.include AttributeDecorators
-      mod.include Locking::Optimistic
-      mod.include Locking::Pessimistic
-      mod.include DefineCallbacks
-      mod.include AttributeMethods
-      # mod.include Callbacks
-      mod.include Timestamp
-      mod.include Associations
-      mod.include ActiveModel::SecurePassword
-      mod.include AutosaveAssociation
-      # mod.include NestedAttributes
-      mod.include Aggregations
-      mod.include Transactions
-      # mod.include TouchLater
-      # mod.include NoTouching
-      mod.include Reflection
-      mod.include Serialization
-      mod.include Store
-      mod.include SecureToken
-      mod.include Suppressor
+      MODULES_EXTENDED.each do |m|
+        mod.extend(m)
+      end
+
+      MODULES_INCLUDED_FIRST.each do |m|
+        mod.include(m)
+      end
+
+      # Unfortunately, this has to go between some of the inclusions.
+      mod.extend(BaseClassFix)
+
+      MODULES_INCLUDED.each do |m|
+        mod.include(m)
+      end
 
       # mod.initialize_generated_modules # AttributeMethods
       mod.initialize_relation_delegate_cache # Delegation::DelegateCache
@@ -66,29 +93,29 @@ module ActiveRecord
       # TODO: Make this more robust. Allow passing table_name as module parameter.
       mod.table_name = mod.name.split("::").first.tableize
 
-      # NOTE: Requires AttributeMethods.
-      def mod.save(entity)
-        new(entity.attributes.transform_keys(&:to_sym)).save
-      end
-
-      # NOTE: Requires AttributeMethods.
-      def mod.save!(entity)
-        new(entity.attributes.transform_keys(&:to_sym)).save!
-      end
-
-      # NOTE: These should probably be protected.
-
-      def mod.where(*args, **kwargs, &block)
-        # TODO: Make this more robust. Allow passing model's class as module parameter.
-        model_class = name.split("::").first.constantize
-        super.map{ |x| model_class.new(x.attributes.transform_keys(&:to_sym)) }
-      end
-
-      def mod.find(id)
-        where(id: id).first
-      end
-
       class << mod
+
+        # NOTE: Requires AttributeMethods.
+        def save(entity)
+          new(entity.attributes.transform_keys(&:to_sym)).save
+        end
+
+        # NOTE: Requires AttributeMethods.
+        def save!(entity)
+          new(entity.attributes.transform_keys(&:to_sym)).save!
+        end
+
+        # NOTE: These should probably be protected.
+
+        def where(*args, **kwargs, &block)
+          # TODO: Make this more robust. Allow passing model's class as module parameter.
+          model_class = name.split("::").first.constantize
+          super.map{ |x| model_class.new(x.attributes.transform_keys(&:to_sym)) }
+        end
+
+        def find(id)
+          where(id: id).first
+        end
 
         # Don't let anyone new up a Repository themselves.
         private :new
